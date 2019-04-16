8bd1790c8c45af71eed7e22dc8117eca8ef1c8cc
augsun <codersun@126.com>
2019-04-16 11:14:56 +0800

    no message

---
+++ Example/Pods/Target Support Files/JXAlipay/JXAlipay-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JXAlipay : NSObject
+@end
+@implementation PodsDummy_JXAlipay
+@end
---
+++ Example/Pods/Target Support Files/Pods-JXAlipay_Example/Pods-JXAlipay_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JXAlipay_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JXAlipay_Example
+@end
